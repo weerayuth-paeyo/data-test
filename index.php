772fb084eb277b0aea364b695335e60250e67d43
weerayuth-paeyo <78775375+weerayuth-paeyo@users.noreply.github.com>
2022-01-25 17:40:03 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,14 @@
-<?php
-echo "Hello";
+<?php 
+$data = array(
+  {
+    "id" => 1,
+    "name" => "Json",
+    "address" => "123/44"
+  },
+  {
+    "id" => 2,
+    "name" => "Divi",
+    "address" => "123/33"
+  }
+        );
+echo jsonencode($data);
